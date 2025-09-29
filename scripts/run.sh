b37e6545e388c5614d1a0b896f06fcbb184f91ec
Adam Ritter <aritter@gmail.com>
2025-09-29 16:07:38 +0200

    Add GitHub API User-Agent

--- scripts/run.sh
+++ scripts/run.sh
@@ -34,6 +34,7 @@ github_api() {
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "User-Agent: pr-preview-action" \
     "$url" "$@"
 }
 
